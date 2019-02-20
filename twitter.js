967ed99ab6ef3413678a84c8b37f35dec57f1845
dheyme-jensen <dheymejensen@hotmail.com>
2019-02-20 18:30:56 -0300

    versao0.0.6

--- twitter.js
+++ twitter.js
@@ -1,32 +1,51 @@
-const counter = 140;
-let tweet = document.querySelector('#tweet');
+const characters = 140;
+let tweetText = document.querySelector('#tweetText');
 let tweetButton = document.querySelector('#tweetButton');
 let printTweet = document.querySelector('#printTweet');
-let contador = document.querySelector('#contador');
-
+let counter = document.querySelector('#counter');
 
 tweetButton.addEventListener('click', listOfTweets);
 
 function listOfTweets() {
-    let text = tweet.value;
+    let text = tweetText.value + moment().calendar();  
     let paragraph = document.createElement('p');
     let message = document.createTextNode(text);
     paragraph.appendChild(message);
-    printTweet.appendChild(paragraph);
-    tweet.value= '';
+    printTweet.appendChild(paragraph); 
+    tweetText.value= '';
 }
- 
-tweet.addEventListener('keyup', disableButton);
 
-function disableButton() {
-    let maxLength = counter - tweet.value.length;
-    if (maxlength === counter || maxLength < 0) {
-        tweetButton.setAttribute("disabled", "");
+tweetText.addEventListener('keyup', disabledButton);
+
+function disabledButton() {
+    let maxLength = characters - tweetText.value.length;
+    counter.textContent = maxLength;
+    if (maxLength === characters || maxLength < 0) {
+      tweetButton.setAttribute("disabled", "");
     } 
     else {
-        tweetButton.removeAttribute("disabled");
-    } 
+     tweetButton.removeAttribute("disabled");
+    }     
+} 
 
-    
+tweetText.addEventListener('keyup', changeColor);
+
+function changeColor(){
+    if(tweetText.value.length < 120 ){
+        counter.setAttribute('class', 'blue');
+    }
+    else if(tweetText.value.length >= 120 && tweetText.value.length <= 130 ){
+        counter.setAttribute('class', 'yellow');
+    }
+    else{
+     counter.setAttribute('class', 'red');
+    }
+} 
+
+tweetText.addEventListener('keyup', expandText);
+
+function expandText(){
+    if(tweetText.scrollHeight > tweetText.offsetHeight) {
+        tweetText.rows += 1;
+    }
 }
-
