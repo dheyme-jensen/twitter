3377a66e074ad5dac119bb8cde2fe2df1895b4d6
dheyme-jensen <dheymejensen@hotmail.com>
2019-02-21 14:31:54 -0300

    css pronto

--- twitter.js
+++ twitter.js
@@ -5,18 +5,23 @@ let printTweet = document.querySelector('#printTweet');
 let counter = document.querySelector('#counter');
 
 tweetButton.addEventListener('click', listOfTweets);
-
 function listOfTweets() {
-    let text = tweetText.value + moment().calendar();  
+    let time = moment().format('LT');
+    let block = `
+        <div class='user'> 
+         <h1>@chandlerbing</h1>
+         <span>${time}</span>
+         <p>${tweetText.value}</p>
+        </div>
+    `;
+    printTweet.innerHTML += block
     let paragraph = document.createElement('p');
     let message = document.createTextNode(text);
     paragraph.appendChild(message);
     printTweet.appendChild(paragraph); 
     tweetText.value= '';
 }
-
 tweetText.addEventListener('keyup', disabledButton);
-
 function disabledButton() {
     let maxLength = characters - tweetText.value.length;
     counter.textContent = maxLength;
@@ -27,23 +32,19 @@ function disabledButton() {
      tweetButton.removeAttribute("disabled");
     }     
 } 
-
 tweetText.addEventListener('keyup', changeColor);
-
 function changeColor(){
     if(tweetText.value.length < 120 ){
         counter.setAttribute('class', 'blue');
     }
     else if(tweetText.value.length >= 120 && tweetText.value.length <= 130 ){
-        counter.setAttribute('class', 'yellow');
+        counter.setAttribute('class', 'orange');
     }
     else{
      counter.setAttribute('class', 'red');
     }
 } 
-
 tweetText.addEventListener('keyup', expandText);
-
 function expandText(){
     if(tweetText.scrollHeight > tweetText.offsetHeight) {
         tweetText.rows += 1;
